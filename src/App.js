f624c72e574907db7364c9fce7b75e22e41b632b
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2023-01-19 17:40:49 +1000

    components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello world!</h2>;
+};
+
+/* const Field = () => {
+  const holder = 'Enter here';
+  const styledField = {
+    width: '300px',
+  };
+
+  return <input placeholder={holder} type='text' style={styledField} />;
+}; */
+
+class Field extends Component {
+  render() {
+    const holder = 'Enter here';
+    const styleField = {
+      width: '300px',
+    };
+
+    return <input placeholder={holder} type='text' style={styleField} />;
+  }
+}
+
+function Btn() {
+  const text = 'Log in';
+  const logged = true;
+  return <button>{logged ? 'Enter' : text}</button>;
+}
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header />
+      <Field />
+      <Btn />
     </div>
   );
 }
