a3d1df2ef2b6bc02f9eae523b7c327b12f6c55cd
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2023-01-20 16:16:04 +1000

    props

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -5,9 +5,9 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 const EmployeesList = () => {
   return (
     <ul className='app-list list-group'>
-      <EmployeesListItem />
-      <EmployeesListItem />
-      <EmployeesListItem />
+      <EmployeesListItem name='John C.' wage={800} />
+      <EmployeesListItem name='Alex M.' wage={3000} />
+      <EmployeesListItem name='Carl W.' wage={5000} />
     </ul>
   );
 };
