f888aee945a8a098329d3d7eebdcfa441834b359
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2023-01-25 12:48:24 +1000

    text search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,6 +17,7 @@ class App extends Component {
         { name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2 },
         { name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3 },
       ],
+      term: '',
     };
     this.maxId = 4;
   }
@@ -46,6 +47,11 @@ class App extends Component {
     });
   };
 
+  getIncreasedEmployeeCount = () => {
+    const newArr = this.state.data.filter((item) => item.increase);
+    return newArr.length;
+  };
+
   onToggleProp = (id, prop) => {
     this.setState(({ data }) => ({
       data: data.map((item) => {
@@ -57,25 +63,37 @@ class App extends Component {
     }));
   };
 
-  getIncreasedEmployeeCount = () => {
-    const newArr = this.state.data.filter((item) => item.increase);
-    return newArr.length;
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+
+    return items.filter((item) => {
+      return item.name.indexOf(term) > -1;
+    });
+  };
+
+  onUpdateSearch = (term) => {
+    this.setState({ term });
   };
 
   render() {
+    const { data, term } = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter((item) => item.increase).length;
+    const visibleData = this.searchEmp(data, term);
+
     return (
       <div className='app'>
         <AppInfo employees={employees} increased={increased} />
 
         <div className='search-panel'>
-          <SearchPanel />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
           <AppFilter />
         </div>
 
         <EmployeesList
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp}
         />
